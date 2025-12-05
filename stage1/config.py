a4146fd6bf08a5177c2f5d1dc28999743d62b9c6
Junhan0518 <65850527+Junhan0518@users.noreply.github.com>
2025-12-05 09:27:22 +0000

    Update config.py

--- stage1/config.py
+++ stage1/config.py
@@ -105,7 +105,7 @@ _C.LOCAL_RANK = 0
 def _update_config_from_file(config, cfg_file):
     config.defrost()
     with open(cfg_file, 'r') as f:
-        yaml_cfg = yaml.load(f, Loader=yaml.FullLoader)
+        yaml_cfg = yaml.safe_load(f, Loader=yaml.FullLoader)
 
     for cfg in yaml_cfg.setdefault('BASE', ['']):
         if cfg:
